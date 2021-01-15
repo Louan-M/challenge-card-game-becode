d2b3ce801e9a0607c8409ad1e061837e73f20608
Louan <louan_mastrogiovanni@outlook.com>
2021-01-15 13:07:27 +0100

    Update code

--- main.py
+++ main.py
@@ -8,5 +8,8 @@
 
 from utils.game import Board
 
+#Players
 list_of_players = ['Louan', 'Theano']
+
+# Starting the game by calling the function start_game() from the Board class
 Board.start_game(2, list_of_players)
