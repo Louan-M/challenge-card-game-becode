5d348448847e334835b339a8fcb49c641ee9d84a
Louan Mastrogiovanni <76942074+Louan-M@users.noreply.github.com>
2021-01-15 15:43:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
-import random
+""" 
+    Start of the program
+
+    :attrib player_list list of player's names
+    :attrib player_name is the name of the player
+    
+"""
+
+from utils.game import Board
+
+# Players
+
+list_of_players = []
+
+print('Please enter your names. Only two players allowed.')
+player_1 = input("First player: ")
+list_of_players.append(player_1)
+player_2 = input("Second player: ")
+list_of_players.append(player_2)
+print(f"\nWelcome {player_1} and {player_2} ! Are you ready ? Let's play!")
 
 
-class Player:
-    """
-    Class containing the Players
-
-    """
-
-    def play(name, cards):
-
-
-        print(cards)
-        
-        input_card = input(f"{name}, please choose a card from the list: ")
-        
-        if input_card in cards:
-            active_card = input_card
-        else:
-            print("The card chosen is not in the list. Please choose a card again: ")
-            active_card = input(f"{name}, please choose a card from the list: ")
-        
-
-        return active_card
-
-
+# Starting the game by calling the function start_game() from the Board class
+Board.start_game(2, list_of_players)
